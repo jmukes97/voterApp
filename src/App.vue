351652082498e6c7238189d7f6c6264f64bab437
Nathan Dimmer <nathandimmer@gmail.com>
2019-09-28 18:11:53 -0400

    Stuff, honestly I dont even know what I've done...

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,11 @@ export default {
 </script>
 
 <style src="./assets/styles.css" lang="css"></style>
+
+<style lang="sass">
+    @import "~vue-material/dist/theme/engine";
+
+    @include md-register-theme("default", (primary: #060051, accent: #BC0000));
+
+    @import "~vue-material/dist/theme/all";
+</style>
--- src/views/UserForm.vue
+++ src/views/UserForm.vue
@@ -5,13 +5,13 @@
         <div class="md-title">Card without hover effect</div>
       </md-card-header>
 
-      <md-card-content>
+      <md-card-content class="md-primary">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
       </md-card-content>
 
       <md-card-actions>
         <md-button>Action</md-button>
-        <md-button>Action</md-button>
+        <md-button class="md-raised md-primary">Primary</md-button>
       </md-card-actions>
     </md-card>
     </div>
@@ -24,14 +24,15 @@ import 'vue-material/dist/vue-material.min.css'
 //import { Person } from 'blockstack'
 //import { userSession } from '../userSession'
 
+
 Vue.use(VueMaterial)
 
 export default {
   name: 'UserForm',
   components: { Vue, VueMaterial },
-  created () {
+  /* created () {
     this.userSession = userSession
-  },
+  }, */
   mounted () {},
   data () {
     return {
@@ -42,8 +43,10 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
     .test {
-        margin: 10%;
+        margin-left: 10%;
+        margin-right: 10%;
     }
 </style>
+
