6bb4bdfac702485c3ba42a0aab16a6d15ad1d3fc
Nathan Dimmer <nathandimmer@gmail.com>
2019-09-28 15:48:01 -0400

    New Packages and User Form veiw

---
+++ src/views/UserForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="center">
+    <landing v-if="! userSession.isUserSignedIn()"></landing>
+    <dashboard v-if="user" :user="user"></dashboard>
+
+  </div>
+</template>
+
+<script>
+import Landing from '@/components/Landing.vue'
+import Dashboard from '@/components/Dashboard.vue'
+import { Person } from 'blockstack'
+import { userSession } from '../userSession'
+
+export default {
+  name: 'Home',
+  components: { Landing, Dashboard },
+  created () {
+    this.userSession = userSession
+  },
+  mounted () {},
+  data () {
+    return {
+      userSession: null,
+      user: null
+    }
+  }
+}
+</script>
