21e6cd0e1cb565a115f0a6c0494700dea2fd4d15
Nathan Dimmer <nathandimmer@gmail.com>
2019-09-29 01:50:31 -0400

    Failed vote page v1

---
+++ src/views/Vote.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <canidate></canidate>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+//import { Person } from 'blockstack'
+//import { userSession } from '../userSession'
+import BootstrapVue from 'bootstrap-vue'
+import Canidate from '@/components/Canidate.vue'
+
+Vue.use(Canidate)
+Vue.use(BootstrapVue)
+Vue.use(VueMaterial)
+
+export default {
+  name: 'Vote',
+  components: { Vue, VueMaterial },
+created () {
+    this.userSession = userSession
+  },
+  mounted () {},
+  data () {
+    return {
+      userSession: null,
+      user: null
+    }
+  }
+}
+</script>
+
+<style>
+    .verticalCenter {
+      position: absolute;
+      top: 50%;
+      -ms-transform: translateY(-50%);
+      transform: translateY(-50%);
+    }
+</style>
