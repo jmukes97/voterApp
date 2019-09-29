064718cf0ef0e002d50fa868678aab36dcafabb6
admin <admin@admins-MacBook-Pro.local>
2019-09-29 05:58:28 -0400

    Added fix for cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // Import the express module and initialize the express app
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 // Imports all the routes from the routes.js file in the routes folder
 const routes = require('./routes/routes.js');
+
+app.use(cors());
 
 // Middleware to utilize the routesin the route.js file
 app.use('/', routes);
