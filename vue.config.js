61bf16847bd7d1bdba43d794680a8cdb9ab9b039
jmukes97 <jmukes@oakland.edu>
2019-09-29 01:43:50 +0000

    Yeet (got backend working)

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  devServer: {
+    devServer: {
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
       'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding'
-    }
+    },
+
   }
 }
