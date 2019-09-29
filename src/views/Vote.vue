076ac478a99fb066fa5042e378640d96ce5b620a
jmukes97 <jmukes@oakland.edu>
2019-09-29 16:52:18 +0000

    Revert "Finish the vote feature???"
    This reverts commit e7093e3f9c062b5dd711439dd29e08565a5240ae.

--- src/views/Vote.vue
+++ src/views/Vote.vue
@@ -11,12 +11,7 @@
                 <div class="md-subhead">{{objArr[0].party}}</div>
               </b-col>
               <b-col>
-                <md-button
-                  class="md-raised md-accent"
-                  :md-ripple="false"
-                  style="float:right;"
-                  onclick="first()"
-                >Vote</md-button>
+                <md-button class="md-raised md-accent" :md-ripple="false" style="float:right;">Vote</md-button>
               </b-col>
             </b-row>
           </md-card-header>
@@ -34,12 +29,7 @@
                 <div class="md-subhead">{{objArr[1].party}}</div>
               </b-col>
               <b-col>
-                <md-button
-                  class="md-raised md-accent"
-                  :md-ripple="false"
-                  style="float:right;"
-                  onclick="second()"
-                >Vote</md-button>
+                <md-button class="md-raised md-accent" :md-ripple="false" style="float:right;">Vote</md-button>
               </b-col>
             </b-row>
           </md-card-header>
@@ -57,12 +47,7 @@
                 <div class="md-subhead">{{objArr[2].party}}</div>
               </b-col>
               <b-col>
-                <md-button
-                  class="md-raised md-accent"
-                  :md-ripple="false"
-                  style="float:right;"
-                  onclick="third()"
-                >Vote</md-button>
+                <md-button class="md-raised md-accent" :md-ripple="false" style="float:right;">Vote</md-button>
               </b-col>
             </b-row>
           </md-card-header>
@@ -79,12 +64,7 @@
                 <div class="md-subhead">{{objArr[3].party}}</div>
               </b-col>
               <b-col>
-                <md-button
-                  class="md-raised md-accent"
-                  :md-ripple="false"
-                  style="float:right;"
-                  onclick="fourth()"
-                >Vote</md-button>
+                <md-button class="md-raised md-accent" :md-ripple="false" style="float:right;">Vote</md-button>
               </b-col>
             </b-row>
           </md-card-header>
@@ -104,7 +84,6 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 import { Person } from "blockstack";
 import { userSession } from "../userSession";
 import BootstrapVue from "bootstrap-vue";
-import axios from "axios";
 
 Vue.use(BootstrapVue);
 Vue.use(VueMaterial);
@@ -116,65 +95,6 @@ export default {
     this.userSession = userSession;
   },
   mounted() {},
-  methods: {
-    methods: {
-      first() {
-        axios
-          .post("https://https://34.68.131.167/test", {
-            name: this.objArr[0].name,
-            election: this.objArr[0].election
-          })
-          .then(function(response) {
-            console.log(response);
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
-      },
-
-      second() {
-        axios
-          .post("https://https://34.68.131.167/test", {
-            name: this.objArr[1].name,
-            election: this.objArr[1].election
-          })
-          .then(function(response) {
-            console.log(response);
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
-      },
-
-      third() {
-        axios
-          .post("https://https://34.68.131.167/test", {
-            name: this.objArr[2].name,
-            election: this.objArr[2].election
-          })
-          .then(function(response) {
-            console.log(response);
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
-      },
-
-      fourth() {
-        axios
-          .post("https://https://34.68.131.167/test", {
-            name: this.objArr[3].name,
-            election: this.objArr[3].election
-          })
-          .then(function(response) {
-            console.log(response);
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
-      }
-    }
-  },
   data() {
     return {
       objArr: [
